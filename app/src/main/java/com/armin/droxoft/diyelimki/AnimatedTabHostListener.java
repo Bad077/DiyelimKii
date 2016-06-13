3e27046ab76e158a44454d62f255039be3b9b44f
Bad0 <htbahadir@gmail.com>
2016-06-13 22:42:26 +0300

    Kategoriler Temel

--- app/src/main/java/com/armin/droxoft/diyelimki/AnimatedTabHostListener.java
+++ app/src/main/java/com/armin/droxoft/diyelimki/AnimatedTabHostListener.java
@@ -15,21 +15,14 @@ public class AnimatedTabHostListener implements TabHost.OnTabChangeListener
     private View currentView;
     private int currentTab;
 
-    /**
-     * Constructor that takes the TabHost as a parameter and sets previousView to the currentView at instantiation
-     *
-     * @param tabHost
-     */
+
     public AnimatedTabHostListener(TabHost tabHost)
     {
         this.tabHost = tabHost;
         this.previousView = tabHost.getCurrentView();
     }
 
-    /**
-     * When tabs change we fetch the current view that we are animating to and animate it and the previous view in the
-     * appropriate directions.
-     */
+
     @Override
     public void onTabChanged(String tabId)
     {
@@ -50,11 +43,6 @@ public class AnimatedTabHostListener implements TabHost.OnTabChangeListener
 
     }
 
-    /**
-     * Custom animation that animates in from right
-     *
-     * @return Animation the Animation object
-     */
     private Animation inFromRightAnimation()
     {
         Animation inFromRight = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 1.0f, Animation.RELATIVE_TO_PARENT, 0.0f,
@@ -62,11 +50,6 @@ public class AnimatedTabHostListener implements TabHost.OnTabChangeListener
         return setProperties(inFromRight);
     }
 
-    /**
-     * Custom animation that animates out to the right
-     *
-     * @return Animation the Animation object
-     */
     private Animation outToRightAnimation()
     {
         Animation outToRight = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 1.0f,
@@ -74,11 +57,6 @@ public class AnimatedTabHostListener implements TabHost.OnTabChangeListener
         return setProperties(outToRight);
     }
 
-    /**
-     * Custom animation that animates in from left
-     *
-     * @return Animation the Animation object
-     */
     private Animation inFromLeftAnimation()
     {
         Animation inFromLeft = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, -1.0f, Animation.RELATIVE_TO_PARENT, 0.0f,
@@ -86,11 +64,6 @@ public class AnimatedTabHostListener implements TabHost.OnTabChangeListener
         return setProperties(inFromLeft);
     }
 
-    /**
-     * Custom animation that animates out to the left
-     *
-     * @return Animation the Animation object
-     */
     private Animation outToLeftAnimation()
     {
         Animation outtoLeft = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, -1.0f,
@@ -98,11 +71,6 @@ public class AnimatedTabHostListener implements TabHost.OnTabChangeListener
         return setProperties(outtoLeft);
     }
 
-    /**
-     * Helper method that sets some common properties
-     * @param animation the animation to give common properties
-     * @return the animation with common properties
-     */
     private Animation setProperties(Animation animation)
     {
         animation.setDuration(ANIMATION_TIME);
