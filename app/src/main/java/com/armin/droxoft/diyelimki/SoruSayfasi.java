df26212ead4ee46fe8a7f4a176db0bbeacdf92ae
Badler01 <leryncold@gmail.com>
2016-06-19 17:03:33 +0300

    button animations_uptated

--- app/src/main/java/com/armin/droxoft/diyelimki/SoruSayfasi.java
+++ app/src/main/java/com/armin/droxoft/diyelimki/SoruSayfasi.java
@@ -3,11 +3,13 @@ package com.armin.droxoft.diyelimki;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 
 public class SoruSayfasi extends Activity {
@@ -29,14 +31,33 @@ public class SoruSayfasi extends Activity {
                 overridePendingTransition(R.anim.slideinleft, R.anim.slideoutright);
             }
         });
-        final Animation ButtonAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_anim_out);
+        final Animation ButtonAnim_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_anim_out);
+        final Animation ButtonAnim_out_late = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_anim__out_late);
+        final Animation ButtonAnim_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_anim_in);
+        final RelativeLayout LayEvetHayir = (RelativeLayout) findViewById(R.id.LayEvetHayir);
+        final RelativeLayout LayStat = (RelativeLayout) findViewById(R.id.layIstatistik);
         final ImageButton evetButton = (ImageButton) findViewById(R.id.bEvet);
         final ImageButton hayirButton = (ImageButton) findViewById(R.id.bHayir);
+        final ImageButton StatButton = (ImageButton) findViewById(R.id.bEvet2);
+        final ImageButton ShareButton = (ImageButton) findViewById(R.id.bHayir2);
         evetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("tago", "bevet");
-                evetButton.startAnimation(ButtonAnim);
+                evetButton.startAnimation(ButtonAnim_out);
+                hayirButton.startAnimation(ButtonAnim_out_late);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        LayEvetHayir.setVisibility(View.INVISIBLE);
+                        LayStat.setVisibility(View.VISIBLE);
+                        StatButton.startAnimation(ButtonAnim_in);
+                        ShareButton.startAnimation(ButtonAnim_in);
+
+                    }
+                }, 800);
+
 
 
 
@@ -47,7 +68,20 @@ public class SoruSayfasi extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i("tago", "bhayir");
-                hayirButton.startAnimation(ButtonAnim);
+                hayirButton.startAnimation(ButtonAnim_out);
+                evetButton.startAnimation(ButtonAnim_out_late);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        LayEvetHayir.setVisibility(View.INVISIBLE);
+                        LayStat.setVisibility(View.VISIBLE);
+                        StatButton.startAnimation(ButtonAnim_in);
+                        ShareButton.startAnimation(ButtonAnim_in);
+
+                    }
+                }, 800);
+
             }
         });
     }
